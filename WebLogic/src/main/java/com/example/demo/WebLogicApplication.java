d443c5bd723927b435b13225ae701f6c4b0c7ee3
rameshacp12429 <rameshacp12429@ASIHPCNU420C47M.BLR.ALLEGISINDIA.COM>
2019-03-26 14:20:13 +0530

    new project

---
+++ WebLogic/src/main/java/com/example/demo/WebLogicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLogicApplication.class, args);
+	}
+
+}
