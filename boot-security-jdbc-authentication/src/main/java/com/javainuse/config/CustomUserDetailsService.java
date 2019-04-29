70c98f9d59e0ed07c3edcf2fe867e5422b49ac50
Rameshameda <48552584+Rameshameda@users.noreply.github.com>
2019-04-29 15:35:28 +0530

    Add files via upload

---
+++ boot-security-jdbc-authentication/src/main/java/com/javainuse/config/CustomUserDetailsService.java
@@ -0,0 +1,67 @@
+package com.javainuse.config;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	@Override
+	public UserDetails loadUserByUsername(final String username)
+			throws UsernameNotFoundException {
+		
+		System.out.println("loadUserByUsername username="+username);
+		
+		return new UserDetails() {
+			
+			private static final long serialVersionUID = 2059202961588104658L;
+
+			@Override
+			public boolean isEnabled() {
+				return true;
+			}
+			
+			@Override
+			public boolean isCredentialsNonExpired() {
+				return true;
+			}
+			
+			@Override
+			public boolean isAccountNonLocked() {
+				return true;
+			}
+			
+			@Override
+			public boolean isAccountNonExpired() {
+				return true;
+			}
+			
+			@Override
+			public String getUsername() {
+				return "rameshmeda";
+			}
+			
+			@Override
+			public String getPassword() {
+				return "rameshmeda";
+			}
+			
+			@Override
+			public Collection<? extends GrantedAuthority> getAuthorities() {
+				List<SimpleGrantedAuthority> auths = new java.util.ArrayList<SimpleGrantedAuthority>();
+				auths.add(new SimpleGrantedAuthority("Admin"));
+				return auths;
+			}
+		};
+	}
+
+}
---
+++ boot-security-jdbc-authentication/src/main/java/com/javainuse/config/EmployeeSecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.javainuse.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class EmployeeSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/welcome").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN").antMatchers("/addNewEmployee")
+				.hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.and().logout().permitAll();
+
+		http.csrf().disable();
+	}
+
+}
